eeab3f80eb57749f09905c5f76bd76b0d61065f5
FrancisRalph <francis341593@gmail.com>
2024-10-28 03:47:13 +0800

    fix errors

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ module "vpc" {
   subnets = [
     {
       subnet_name           = "actual-subnet"
-      subnet_ip            = "10.10.10.0/24"
-      subnet_region        = var.region
+      subnet_ip             = "10.10.10.0/24"
+      subnet_region         = var.region
       subnet_private_access = true
     }
   ]
@@ -26,9 +26,9 @@ module "cloud_run" {
   source  = "GoogleCloudPlatform/cloud-run/google//modules/v2"
   version = "~> 0.13.0"
 
-  project_id    = var.project_id
-  location      = var.region
-  service_name  = "actual-server"
+  project_id   = var.project_id
+  location     = var.region
+  service_name = "actual-server"
 
   containers = [
     {
@@ -39,21 +39,22 @@ module "cloud_run" {
   vpc_access = {
     egress = "all-traffic"
     network_interfaces = {
-      network = module.vpc.network_name
+      network    = module.vpc.network_name
       subnetwork = module.vpc.subnets_names[0]
     }
   }
 
-  volumes = {
+  volumes = [{
+    name = "actual-server-data"
     gcs = {
       bucket = var.gcs_bucket_name
     }
-  }
+  }]
 }
 
 module "gcs_buckets" {
-  source  = "terraform-google-modules/cloud-storage/google"
-  version = "~> 8.0"
-  project_id  = var.project_id
-  names = [var.gcs_bucket_name]
+  source     = "terraform-google-modules/cloud-storage/google"
+  version    = "~> 8.0"
+  project_id = var.project_id
+  names      = [var.gcs_bucket_name]
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "service_url" {
-  value       = module.cloud_run.service_url
+output "service_uri" {
+  value       = module.cloud_run.service_uri
   description = "The URL on which the deployed service is available"
 }
 
