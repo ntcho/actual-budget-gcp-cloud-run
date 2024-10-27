b446b42139fb28c41ed142ba845d392c40fdfc29
FrancisRalph <francis341593@gmail.com>
2024-10-28 06:01:24 +0800

    fix billing service disabled error

--- main.tf
+++ main.tf
@@ -26,14 +26,14 @@ module "cloud_run" {
   source  = "GoogleCloudPlatform/cloud-run/google//modules/v2"
   version = "~> 0.13.0"
 
-  project_id   = var.project_id
-  location     = var.region
-  service_name = "actual-server"
+  project_id                    = var.project_id
+  location                      = var.region
+  service_name                  = "actual-server"
   service_account_project_roles = ["roles/storage.admin"]
 
   max_instance_request_concurrency = 5
-  ingress = "INGRESS_TRAFFIC_ALL"
-  
+  ingress                          = "INGRESS_TRAFFIC_ALL"
+
   template_scaling = {
     max_instance_count = 1
   }
@@ -42,7 +42,7 @@ module "cloud_run" {
     {
       container_image = "actualbudget/actual-server:latest"
       ports = {
-        name = "http1"
+        name           = "http1"
         container_port = 5006
       }
     }
--- provider.tf
+++ provider.tf
@@ -1,9 +1,16 @@
-terraform { 
-  cloud { 
-    organization = "Actual-Budget-Personal" 
+terraform {
+  cloud {
+    organization = "Actual-Budget-Personal"
 
-    workspaces { 
-      name = "self" 
-    } 
-  } 
+    workspaces {
+      name = "self"
+    }
+  }
+}
+
+provider "google" {
+  project               = var.project_id
+  region                = var.region
+  billing_project       = var.project_id
+  user_project_override = true
 }
